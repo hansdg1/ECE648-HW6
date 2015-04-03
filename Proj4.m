65091b6b0bbf74fee1a709d14f0a0798830cf286
Hans Guthrie <hansdg1+github@gmail.com>
2015-04-03 13:57:26 -0500

    Maybe working. Turning in

--- Proj4.m
+++ Proj4.m
@@ -63,48 +63,51 @@ fprintf('MSE for zonal_4: %.2f\n', zonal_4.mse);
 fprintf('MSE for zonal_9: %.2f\n', zonal_9.mse);
 fprintf('MSE for zonal_16: %.2f\n', zonal_16.mse);
 
-saveFigs(fig2, 'ZonalSampling.png', 'png');
 
 %% 1.3 Hadamard
 
-% H = (1/sqrt(8)) * hadamard(blocksize(1));
-% 
-% fun = @(block_struct) hadamard(block_struct.data);
-% lenaH = blockproc(lena_original, blocksize, fun);
-% 
-% 
-
 H = (1/sqrt(8))*hadamard(8);
+Zero = zeros(8);
+Mask4 = Zero;
+Mask9 = Zero;
+Mask16 = Zero;
+Mask4(1:2, 1:2) = 1;
+Mask9(1:3, 1:3) = 1;
+Mask16(1:4, 1:4) = 1;
 
 % 4 Coefficients
-% lenaH = blkproc(lena_original, [8,8], 'P1*x*P2', H, H');
-% fun = @(block_struct) zonal_4.maskcoeff .* block_struct.data;
-% lena4H = blockproc(lenaH, [8 8], fun);
-% lena4H = blkproc(lena4H, [8,8], 'P1*x*P2', H, H');
-figure(3)
-hadamard4 = hadamard_transform(lena_original, blocksize, 4);
-imshow(hadamard4,[]);
-% 
-% subplot(3, 3, 3);
-% imshow(lena4H, []);
-% title('4 Hadamard Coefficients')
-% 
-% % 9 Coefficients
-% lenaH = blkproc(lena_original, [8,8], 'P1*x*P2', H, H');
-% fun = @(block_struct) zonal_9.maskcoeff .* block_struct.data;
-% lena9H = blockproc(lenaH, [8 8], fun);
-% lena9H = blkproc(lena9H, [8,8], 'P1*x*P2', H, H');
-% 
-% subplot(3, 3, 6);
-% imshow(lena9H, []);
-% title('9 Hadamard Coefficients')
-% 
-% % 9 Coefficients
-% lenaH = blkproc(lena_original, [8,8], 'P1*x*P2', H, H');
-% fun = @(block_struct) zonal_16.maskcoeff .* block_struct.data;
-% lena16H = blockproc(lenaH, [8 8], fun);
-% lena16H = blkproc(lena16H, [8,8], 'P1*x*P2', H, H');
-% 
-% subplot(3, 3, 9);
-% imshow(lena16H, []);
-% title('16 Hadamard Coefficients')
+lenaH = blkproc(lena_original, [8,8], 'P1*x*P2', H, H');
+fun = @(block_struct) Mask4.*block_struct.data;
+lena4H = blockproc(lenaH, [8 8], fun);
+lena4H = blkproc(lena4H, [8,8], 'P1*x*P2', H, H');
+
+subplot(3, 3, 3);
+imshow(lena4H, []);
+title('4 Hadamard Coefficients')
+
+% 9 Coefficients
+lenaH = blkproc(lena_original, [8,8], 'P1*x*P2', H, H');
+fun = @(block_struct) Mask9.*block_struct.data;
+lena9H = blockproc(lenaH, [8 8], fun);
+lena9H = blkproc(lena9H, [8,8], 'P1*x*P2', H, H');
+
+subplot(3, 3, 6);
+imshow(lena9H, []);
+title('9 Hadamard Coefficients')
+
+% 9 Coefficients
+lenaH = blkproc(lena_original, [8,8], 'P1*x*P2', H, H');
+fun = @(block_struct) Mask16.*block_struct.data;
+lena16H = blockproc(lenaH, [8 8], fun);
+lena16H = blkproc(lena16H, [8,8], 'P1*x*P2', H, H');
+
+subplot(3, 3, 9);
+imshow(lena16H, []);
+title('16 Hadamard Coefficients')
+
+fprintf('MSE for Hadamard 4: %.2f\n', meanSquaredError(lena_original, lena4H));
+fprintf('MSE for Hadamard 9: %.2f\n', meanSquaredError(lena_original, lena9H));
+fprintf('MSE for Hadamard 16: %.2f\n', meanSquaredError(lena_original, lena16H));
+
+
+saveFigs(fig2, 'ZonalSampling.png', 'png');
